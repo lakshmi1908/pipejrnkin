3bbcef80cad3699f3e1b10fed27339afba79fcd4
lakshmi1908 <112061000+lakshmi1908@users.noreply.github.com>
2022-09-10 17:14:38 +0530

    Create JavaExample.java

---
+++ JavaExample.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class JavaExample {
+
+    public static void main(String[] args) {
+        String str="LAKSHMI";
+        String reversed = reverseString(str);
+        System.out.println("The reversed string is: " + reversed);
+    }
+
+    public static String reverseString(String str)
+    {
+        if (str.isEmpty())
+            return str;
+        //Calling Function Recursively
+        return reverseString(str.substring(1)) + str.charAt(0);
+    }
+}
